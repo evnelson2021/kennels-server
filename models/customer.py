9db8239f6d56623fd2f063ab7741ec7aa95bf30a
Emily Nelson <evnelson2021@gmail.com>
2023-01-09 15:44:39 -0600

    EOD 1/9 - finished column 1

---
+++ models/customer.py
@@ -0,0 +1,6 @@
+class Customer():
+
+    def __init__(self, id, email, full_name):
+        self.id = id
+        self.email = email
+        self.full_name = full_name
--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,6 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from views import get_all_animals, get_single_animal, get_all_locations, get_single_location, get_all_employees, get_single_employee, get_all_customers, get_single_customer, create_animal, create_location, create_employee, create_customer, delete_animal
+from views import get_all_animals, get_single_animal, get_all_locations, get_single_location, get_all_employees, get_single_employee, get_all_customers, get_single_customer, create_animal, create_location, create_employee, create_customer, delete_animal, delete_customer, delete_employee, delete_location, update_animal, update_customer, update_employee, update_location
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -126,15 +126,51 @@ class HandleRequests(BaseHTTPRequestHandler):
         if resource == "animals":
             delete_animal(id)
 
+        if resource == "customers":
+            delete_customer(id)
+
+        if resource == "employees":
+            delete_employee(id)
+
+        if resource == "locations":
+            delete_location(id)
+
         # Encode the new animal and send in response
         self.wfile.write("".encode())
 
         
+        
+    def do_PUT(self):
+        self._set_headers(204)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            update_animal(id, post_body)
+
+        if resource == "customers":
+            update_customer(id, post_body)
+
+        if resource == "employees":
+            update_employee(id, post_body)
+
+        if resource == "locations":
+            update_location(id, post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
+
+
 
     # A method that handles any PUT request.
-    def do_PUT(self):
-        """Handles PUT requests to the server"""
-        self.do_PUT()
+    # def do_PUT(self):
+    #     """Handles PUT requests to the server"""
+    #     self.do_PUT()
 
     def _set_headers(self, status):
         # Notice this Docstring also includes information about the arguments passed to the function
